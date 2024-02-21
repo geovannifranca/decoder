96bde054b2324e34e291af7d0d06942deb09996c
geovannifranca <geovannidefranca98@gmail.com>
2024-02-21 12:07:11 -0300

    adicionando js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('textarea').value;
+const btnEncrypt = document.querySelector('.button-encrypt');
+const typeAText = document.querySelector('.type-a-text');
+const textOutput = document.querySelector('.text-output');
+const decryptedText = document.querySelector('.decrypted-text');
+
+
+
+function encrypt(text) {
+    const mapEncrypt = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    return text.split('').map(char => {
+        const crypto = mapEncrypt.find(([letter]) => letter === char);
+        return crypto ? crypto[1] : char;
+    }).join('');
+}
+
+function decrypt(text) {
+    const mapDecrypt = [['enter', 'e'], ['imes', 'i'], ['ai', 'a'], ['ober', 'o'], ['ufat', 'u']];
+    return text.replace(/(enter|imes|ai|ober|ufat)/g, match => {
+        const description = mapDecrypt.find(([crypto]) => crypto === match);
+        return description ? description[1] : match;
+    });
+}
+
+btnEncrypt.addEventListener("click", ()=>{
+    encrypt(input);
+    typeAText.style.display = 'none';
+    textOutput.style.display = 'flex';
+    decryptedText.innerHTML = `${encrypt(input)}`;
+
+});
+
+console.log(encrypt(input));
+console.log(encrypt('gato'))
+console.log(decrypt('gaitober'))
